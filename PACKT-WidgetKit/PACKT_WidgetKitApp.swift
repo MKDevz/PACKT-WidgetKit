db53a9ca4b7ddaa7054920d4932414a27bf4eccc
Mohammad Alkandari <Mohammad.Alkandari@WARBABANK.COM>
2023-09-13 13:27:09 +0300

    Initial Commit

---
+++ PACKT-WidgetKit/PACKT_WidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PACKT_WidgetKitApp.swift
+//  PACKT-WidgetKit
+//
+//  Created by Warba on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PACKT_WidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
